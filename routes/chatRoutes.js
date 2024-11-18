4aa15f799c20c1a3e496e29995d6a4f4a30affea
NeerajNero <neerajnero9066@gmail.com>
2024-11-18 10:23:32 +0530

    chore: edited chatRoutes had some error

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const checkAuthToken = require('../middleware/checkToken')
+const checkAuthToken = require('../middleware/checkAuth')
 const privateRoute = require('../controllers/privateController')
 
 router.get('/chat', checkAuthToken, privateRoute)
