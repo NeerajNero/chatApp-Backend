db2f195212d6983ff690b717d26d74797c60910d
NeerajNero <neerajnero9066@gmail.com>
2024-11-18 10:03:01 +0530

    chore: modified files for deployed frontend

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ initializeDatabase()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chat-app-frontend-one-ruddy.vercel.app/',
         credentials: true
     }
 });
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-frontend-one-ruddy.vercel.app/',
     credentials: true
 }))
 app.use(cookieParser())
